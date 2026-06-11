90751785a236cfd8b2e115a9a864a7af69948739
Vansh Garg <134198050+Vanshgargji@users.noreply.github.com>
2026-06-11 12:31:59 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,58 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& vis, vector<vector<int>>& isConnected) {
-        vis[node] = 1;
+    class DisjointSet{
+        public:
+        vector<int> rank, parent;
 
-        for (int i = 0; i < isConnected[node].size(); i++) {
-            if (isConnected[node][i] == 1 && !vis[i]) {
-                dfs(i, vis, isConnected);
+        DisjointSet(int n){
+            rank.resize(n+1, 0);
+            parent.resize(n+1);
+
+            for(int i=0; i<n; i++){
+                parent[i] = i;
             }
         }
-    }
+
+        int findUPar(int node){
+            if(node == parent[node]) return node;
+
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        void unionByRank(int u, int v){
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+            if(ulp_u == ulp_v) return;
+
+            if(rank[ulp_u] < rank[ulp_v]){
+                parent[ulp_u] = ulp_v;
+            }
+            else if(rank[ulp_v] < rank[ulp_u]){
+                parent[ulp_v] = ulp_u;
+            }
+            else{
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+    };
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> vis(n, 0);
-        int count = 0;
+        DisjointSet ds(n);
 
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                dfs(i, vis, isConnected);
-                count++;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(isConnected[i][j] == 1)
+                ds.unionByRank(i, j);
             }
         }
+        
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(ds.parent[i] == i) cnt++;
+        }
 
-        return count;
+        return cnt;
     }
 };
