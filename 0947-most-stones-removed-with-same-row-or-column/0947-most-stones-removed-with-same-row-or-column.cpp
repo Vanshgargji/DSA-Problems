4eb1e9b075a861a81a3de92d6ec93f505bff1d7b
Vansh Garg <134198050+Vanshgargji@users.noreply.github.com>
2026-06-13 17:37:02 +0530

    Time: 51 ms (25.27%), Space: 21 MB (61.27%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,36 +1,66 @@
 class Solution {
 public:
-    void dfs(int node, vector<bool> &visited, vector<vector<int>> &adjList){
-        visited[node] = true;
+class DisjointSet{
+        public:
+        vector<int> rank, parent;
 
-        for(auto &it : adjList[node]){
-            if(!visited[it]){
-                dfs(it, visited, adjList);
+        DisjointSet(int n){
+            rank.resize(n+1, 0);
+            parent.resize(n+1);
+
+            for(int i=0; i<n; i++){
+                parent[i] = i;
             }
         }
-    }
+
+        int findUPar(int node){
+            if(node == parent[node]) return node;
+
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        void unionByRank(int u, int v){
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+            if(ulp_u == ulp_v) return;
+
+            if(rank[ulp_u] < rank[ulp_v]){
+                parent[ulp_u] = ulp_v;
+            }
+            else if(rank[ulp_v] < rank[ulp_u]){
+                parent[ulp_v] = ulp_u;
+            }
+            else{
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+    };
 
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        vector<vector<int>> adjList(n);
+
+        DisjointSet ds(n);
+        vector<vector<int>> edges;
 
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+                    edges.push_back({i, j});
                 }
             }
         }
 
-        int components = 0;
-        vector<bool> visited(n, false);
-
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                dfs(i, visited, adjList);
-                components++;
+        for(auto &it : edges){
+            int u = it[0], v = it[1];
+            if(ds.findUPar(u) != ds.findUPar(v)){
+                ds.unionByRank(u, v);
             }
+        }
+
+        int components = 0;
+        for(int i=0; i<n; i++){
+            if(ds.parent[i] == i) components++;
         }
 
         return n - components;
