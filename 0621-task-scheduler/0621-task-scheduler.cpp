80ff49b646c41fe9ef5e7f3f5d76fce5f1b7b8c8
Vansh Garg <134198050+Vanshgargji@users.noreply.github.com>
2026-06-17 09:44:34 +0530

    Time: 81 ms (6.46%), Space: 47.8 MB (19.72%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> mp(26, 0);
+
+        for(auto &ch : tasks){
+            mp[ch - 'A']++;
+        }
+
+        int time  = 0;
+        priority_queue<int> pq; 
+
+        for(int i=0; i<26; i++){
+            if(mp[i] > 0){
+                pq.push(mp[i]);
+            }
+        }
+
+        while(!pq.empty()){
+            vector<int> temp;
+
+            for(int i=1; i <= n+1; i++){
+                if(!pq.empty()){
+                    int freq = pq.top();
+                    pq.pop();
+                    freq--;
+
+                    temp.push_back(freq);
+                }
+            }
+
+            for(auto &it : temp){
+                if(it > 0){
+                    pq.push(it);
+                }
+            }
+
+            if(!pq.empty()){    // tasks remains 
+                time += n + 1;
+            }
+            else {              // final step when time is taken according to left tasks
+                time += temp.size();
+            }
+        }
+        
+        return time;
+    }
+};
