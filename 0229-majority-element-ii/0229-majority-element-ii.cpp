d6dd1a212b48d1a272a2ee1ed8e25295c17e9c04
Vansh Garg <134198050+Vanshgargji@users.noreply.github.com>
2026-07-03 16:23:22 +0530

    Time: 0 ms (100%), Space: 23.6 MB (63.75%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,21 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> mp;
+        int cnt1 = 0, cnt2 = 0, ele1 = INT_MIN, ele2 = INT_MIN;
 
-        for(auto &it : nums){
-            mp[it]++;
+        for(auto &num : nums){
+            if(cnt1 == 0 && num != ele2){
+                ele1 = num;
+                cnt1++;
+            }
+            else if(cnt2 == 0 && num != ele1){
+                ele2 = num;
+                cnt2++;
+            }
+            else if(num == ele1){
+                cnt1++;
+            }
+            else if(num == ele2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0, cnt2 = 0;
+        for(auto &i : nums){
+            if(ele1 == i) cnt1++;
+            else if(ele2 == i) cnt2++;
         }
 
         vector<int> ans;
-        for(auto &p : mp){
-            if(ans.size() == 2) break;
-
-            if(p.second > n/3){
-                ans.push_back(p.first);
-            }
-        }
-        
+        if(cnt1 > n/3) ans.push_back(ele1);
+        if(cnt2 > n/3 && ele1 != ele2) ans.push_back(ele2);
 
         return ans;
     }
