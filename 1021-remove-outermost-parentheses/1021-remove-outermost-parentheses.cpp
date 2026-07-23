af5b17df084502f7120bfb9fcbeae39d5131024f
Vansh Garg <134198050+Vanshgargji@users.noreply.github.com>
2026-07-23 16:46:02 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.81%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans = "";
+        string ans  = "";
         int balanced = 0;
-        stack<char> st;
 
-        for(int i=0; i<s.size(); i++){
-            st.push(s[i]);
-            if(s[i] == '(') balanced++;
-            else balanced--;
-            
-            if(balanced == 0){
-                st.pop();
-                string part = "";
-                while(st.size() > 1){
-                    part += st.top();
-                    st.pop();
-                }
-                st.pop();
-                reverse(part.begin(), part.end());
-                ans += part;
+        for(auto &c : s){
+            if(c == '('){
+                if(balanced > 0) ans += c;
+                balanced++;
+            }
+            else{
+                balanced--;
+                if(balanced > 0) ans += c;
             }
         }
+
         return ans;
     }
 };
