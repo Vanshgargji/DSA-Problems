f39bbc1e8944bf0edd03ba647b67c454bee15cf3
Vansh Garg <134198050+Vanshgargji@users.noreply.github.com>
2026-08-01 10:39:19 +0530

    Time: 0 ms (100%), Space: 18.3 MB (43.83%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    int m , n;
-
-    void dfs(int row, int col, vector<vector<int>> &image2, vector<vector<int>> &vis, int color, int val){
-        vis[row][col] = 1;
-        image2[row][col] = color;
-
-        int drow[] = {0, -1, 0, +1};
-        int dcol[] = {-1, 0, +1, 0};
-
-        for(int i=0; i<4; i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-
-            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && image2[nrow][ncol] == val && !vis[nrow][ncol]) dfs(nrow, ncol, image2, vis, color, val);
-        }
-    }
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        m = image.size();
-        n = image[0].size();
+        // edge case
+        if(color == image[sr][sc]){
+            return image;
+        }
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
+        int r = image.size();
+        int c = image[0].size();
 
-        vector<vector<int>> image2(m, vector<int>(n, 0));
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                image2[i][j] = image[i][j];
+        int originalColor = image[sr][sc];
+        image[sr][sc] = color;
+
+        queue<pair<int, int>> q;
+
+        q.push({sr, sc});
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+
+            q.pop();
+            // left up right down
+            int drow[] = {0, -1, 0, +1};
+            int dcol[] = {-1, 0, +1, 0};
+
+            for(int i=0; i<4; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if(nrow >=0 && nrow < r && ncol >= 0 && ncol < c && image[nrow][ncol] == originalColor){
+                    q.push({nrow, ncol});
+                    image[nrow][ncol] = color;
+                }
             }
         }
 
-        dfs(sr, sc, image2, vis, color, image[sr][sc]);
-        return image2;
+        return image;
     }
 };
